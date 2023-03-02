11dce874b35b8b764808bdd247a1cdb61442c923
zipmic <mail@code.dk>
2023-03-02 12:14:24 +0100

    To players kan spawn

--- Assets/TestScene/TestInputSystem.cs
+++ Assets/TestScene/TestInputSystem.cs
@@ -12,18 +12,21 @@ public class TestInputSystem : MonoBehaviour
 	private void Awake()
 	{
 		_rb = GetComponent<Rigidbody>();
+		/*
 		_playerInputActions = new PlayerInputActions();
 		_playerInputActions.Player.Enable();
 		_playerInputActions.Player.Jump.performed += Jump;
-	
+		*/
+
 
 	}
 
 	private void Update()
 	{
-		Vector2 inputVector = _playerInputActions.Player.Movement.ReadValue<Vector2>();
+	/*	Vector2 inputVector = _playerInputActions.Player.Movement.ReadValue<Vector2>();
 		float speed = 5f;
 		_rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+	*/
 	}
 
 	public void Jump(InputAction.CallbackContext context)
@@ -31,14 +34,14 @@ public class TestInputSystem : MonoBehaviour
         //print("Jump!" + context.phase);
 		if (context.performed)
 		{
-			_rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+			_rb.AddForce(Vector3.up * 15f, ForceMode.Impulse);
 		}
     }
 
 	public void Movement(InputAction.CallbackContext context)
 	{
 		Vector2 inputVector = context.ReadValue<Vector2>();
-		float speed = 5f;
+		float speed = 15f;
 		_rb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
 	}
 }
